2bdfc15ced8d2d43fbb5c3549c25d635e2db92f7
predeep kumar singroul <prasingroul4899@gmail.com>
2023-09-27 07:08:31 +0530

    Doctor

--- DoctorManagement/src/main/java/com/health/Entities/Doctor.java
+++ DoctorManagement/src/main/java/com/health/Entities/Doctor.java
@@ -1,5 +1,19 @@
 package com.health.Entities;
 
+import com.health.Enums.Speciality;
+
 public class Doctor {
+	
+	private String name;
+	
+	private String city;
+	
+	private String email;
+	
+	private String password;
+	
+	private String phoneNo;
+	
+	private Speciality speciality;
 
 }
---
+++ DoctorManagement/src/main/java/com/health/Enums/City.java
@@ -0,0 +1,5 @@
+package com.health.Enums;
+
+public enum City {
+
+}

3f6609652d4356e6393042deb54e3abbf51f23eb
predeep kumar singroul <prasingroul4899@gmail.com>
2023-09-27 14:56:08 +0530

    patient repository

--- DoctorManagement/src/main/java/com/health/Repository/DoctorDAO.java
+++ DoctorManagement/src/main/java/com/health/Repository/DoctorDAO.java
@@ -1,8 +1,11 @@
 package com.health.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.health.Entities.Doctor;
@@ -11,7 +14,10 @@ import com.health.Entities.Doctor;
 public interface DoctorDAO extends JpaRepository<Doctor, Integer>{
 	
 	Optional<Doctor> findByEmail(String email);
-
+	
 	Optional<Doctor> findByEmailAndPassword(String email,String password);
+	
+	@Query(value = "SELECT * FROM doctor d WHERE d.city =:city",nativeQuery=true)
+	List<Doctor> getDoctorBasedOnLocation(@Param("city") String city);
 
 }

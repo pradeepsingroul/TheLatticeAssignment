6d609c9bfb83b3592315e29fd685130568e126a8
predeep kumar singroul <prasingroul4899@gmail.com>
2023-09-27 12:47:59 +0530

    patient data access layer

--- DoctorManagement/src/main/java/com/health/Repository/PatientDAO.java
+++ DoctorManagement/src/main/java/com/health/Repository/PatientDAO.java
@@ -1,11 +1,17 @@
 package com.health.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.health.Entities.Doctor;
 import com.health.Entities.Patient;
 
 @Repository
 public interface PatientDAO extends JpaRepository<Patient, Integer>{
+	
+	Optional<Patient> findByEmail(String email);
 
+	Optional<Patient> findByEmailAndPassword(String email,String password);
 }

7d4db74d39f7862aeda8b126082cdbfd1a16835c
predeep kumar singroul <prasingroul4899@gmail.com>
2023-09-27 14:55:01 +0530

    Suggession api implementation

--- DoctorManagement/src/main/java/com/health/ServicesImplementations/PatientServiceIMPL.java
+++ DoctorManagement/src/main/java/com/health/ServicesImplementations/PatientServiceIMPL.java
@@ -1,32 +1,38 @@
 package com.health.ServicesImplementations;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.result.Output;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.health.DTO.LoginDTO;
+import com.health.Entities.Doctor;
 import com.health.Entities.Patient;
+import com.health.Enums.Symtoms;
+import com.health.Exceptions.DoctorExceptions;
 import com.health.Exceptions.PatientExceptions;
+import com.health.Repository.DoctorDAO;
 import com.health.Repository.PatientDAO;
 import com.health.Services.PatientServices;
 
-
-
 @Service
-public class PatientServiceIMPL implements PatientServices{
+public class PatientServiceIMPL implements PatientServices {
 
 	@Autowired
 	private PatientDAO pDao;
-	
+
+	@Autowired
+	private DoctorDAO dDao;
 
 	@Override
 	public Patient register(Patient patient) throws PatientExceptions {
 		Optional<Patient> opt = pDao.findByEmail(patient.getEmail());
-		if(opt.isPresent()) {
-			throw new PatientExceptions("patient already exist with the mail:"+patient.getEmail());
-		}else {
+		if (opt.isPresent()) {
+			throw new PatientExceptions("patient already exist with the mail:" + patient.getEmail());
+		} else {
 			return pDao.save(patient);
 //			return d1;/
 		}
@@ -34,21 +40,22 @@ public class PatientServiceIMPL implements PatientServices{
 
 	@Override
 	public Patient login(LoginDTO Logindto) throws PatientExceptions {
-		Optional<Patient> opt = pDao.findByEmailAndPassword(Logindto.getEmail(),Logindto.getPassword());
-		if(opt.isEmpty()) {
-			throw new PatientExceptions("patient does not exist with the email:"+Logindto.getEmail()+ " please registe first");
-		}else {
+		Optional<Patient> opt = pDao.findByEmailAndPassword(Logindto.getEmail(), Logindto.getPassword());
+		if (opt.isEmpty()) {
+			throw new PatientExceptions(
+					"patient does not exist with the email:" + Logindto.getEmail() + " please registe first");
+		} else {
 			return opt.get();
 		}
-		
+
 	}
 
 	@Override
 	public Patient getPatientEmail(String email) throws PatientExceptions {
 		Optional<Patient> opt = pDao.findByEmail(email);
-		if(opt.isEmpty()) {
-			throw new PatientExceptions("patient does not exist with the email:"+email);
-		}else {
+		if (opt.isEmpty()) {
+			throw new PatientExceptions("patient does not exist with the email:" + email);
+		} else {
 			return opt.get();
 		}
 	}
@@ -56,9 +63,9 @@ public class PatientServiceIMPL implements PatientServices{
 	@Override
 	public List<Patient> getAllPatient() throws PatientExceptions {
 		List<Patient> list = pDao.findAll();
-		if(list.isEmpty()) {
+		if (list.isEmpty()) {
 			throw new PatientExceptions("Patient does not exist ");
-		}else {
+		} else {
 			return list;
 		}
 	}
@@ -66,13 +73,74 @@ public class PatientServiceIMPL implements PatientServices{
 	@Override
 	public Patient deleteByEmail(String email) throws PatientExceptions {
 		Optional<Patient> opt = pDao.findByEmail(email);
-		if(opt.isEmpty()) {
-			throw new PatientExceptions("Doctor does not exist with the email: "+email);
-		}else {
+		if (opt.isEmpty()) {
+			throw new PatientExceptions("Patient does not exist with the email: " + email);
+		} else {
 			Patient p = opt.get();
 			pDao.delete(p);
 			return p;
 		}
+	}
+
+	@Override
+	public List<Doctor> getSuggessioins(String email) throws DoctorExceptions, PatientExceptions {
+		Optional<Patient> opt = pDao.findByEmail(email);
+
+		if (opt.isEmpty()) {
+			throw new PatientExceptions("Patient does not exist with the email: " + email);
+		} else {
+			Patient p = opt.get();
+			Symtoms symtoms = p.getSymtomes();
+			String city = p.getCity();
+			System.out.println(symtoms.toString().equals("Arthritis"));
+
+			List<Doctor> doctors = dDao.getDoctorBasedOnLocation(city);
+			if (doctors.isEmpty()) {
+				throw new DoctorExceptions("We are still waiting to expand to your location");
+			} else {
+				List<Doctor> output = new ArrayList<>();
+				for (Doctor doctor : doctors) {
+
+					if (symtoms.toString().equals("Arthritis") || symtoms.toString().equals("BackPain")
+							|| symtoms.toString().equals("TissueInjuries")) {
+						if (doctor.getSpeciality().toString().equals("ORHTOPEDIC")) {
+							output.add(doctor);
+						} else {
+							throw new DoctorExceptions(
+									"There isn’t any doctor present at your location for your symptom");
+						}
+					} else if (symtoms.toString().equals("Dysmenorrhea")) {
+						if (doctor.getSpeciality().toString().equals("GYNECOLOGY")) {
+							output.add(doctor);
+						} else {
+							throw new DoctorExceptions(
+									"There isn’t any doctor present at your location for your symptom");
+						}
+					} else if (symtoms.toString().equals("SkinInfection") || symtoms.toString().equals("SkinBurn")) {
+						if (doctor.getSpeciality().toString().equals("DERMATOLOGY")) {
+							output.add(doctor);
+						} else {
+							throw new DoctorExceptions(
+									"There isn’t any doctor present at your location for your symptom");
+						}
+					} else if (symtoms.toString().equals("EarPain")) {
+						if (doctor.getSpeciality().toString().equals("ENT")) {
+							output.add(doctor);
+						} else {
+							throw new DoctorExceptions(
+									"There isn’t any doctor present at your location for your symptom");
+						}
+					} else {
+						throw new DoctorExceptions("There isn’t any doctor present at your location for your symptom");
+					}
+				}
+//			System.out.println("dgfdfgd");
+//			System.out.println(doctors);
+				System.out.println(output);
+				return output;
+			}
+		}
+
 	}
 
 }

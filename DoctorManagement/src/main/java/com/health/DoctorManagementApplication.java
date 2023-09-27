d9d01bbcdab28394f870c559c96bc7b1b4559d67
predeep kumar singroul <prasingroul4899@gmail.com>
2023-09-27 14:57:54 +0530

    Doctors management system

--- DoctorManagement/src/main/java/com/health/DoctorManagementApplication.java
+++ DoctorManagement/src/main/java/com/health/DoctorManagementApplication.java
@@ -3,10 +3,14 @@ package com.health;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.health.Enums.Speciality;
+import com.health.Enums.Symtoms;
+
 @SpringBootApplication
 public class DoctorManagementApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DoctorManagementApplication.class, args);
 	}
 
